eeab5204d11c84635308f9a4390f9e32feecdb24
kristiyanCgn <101466719+kristiyanCgn@users.noreply.github.com>
2022-10-15 14:05:20 +0200

    Basic functionality and main css added

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+
+import './App.css';
+
+import { Board } from './Components/Board';
+
+function App() {
+
+    const WINNING_CONDITIONS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [xTurn, setXTurn] = useState(true)
+
+    const handleBoxClick = (boxIdx) => {
+        const updatedBoard = board.map((value, i) => {
+            if(i === boxIdx) {
+                return xTurn === true ? "X" : "O"
+            } else {
+                return value;
+            }
+        })
+
+        checkWInner(updatedBoard);
+
+        setBoard(updatedBoard);
+
+        setXTurn(!xTurn);
+    }
+
+    const checkWInner = (board) => {
+        for(let i = 0; i < WINNING_CONDITIONS.length; i++) {
+            const [x,y,z] = WINNING_CONDITIONS[i]
+
+            if(board[x] && board[x] === board[y] && board[y] === board[z]) {
+                console.log("X");
+                return board[x];
+            }
+        }
+    }
+
+  return (
+    <div className="App">
+        <Board board={board} onClick={handleBoxClick} />
+    </div>
+  );
+}
+
+export default App;
